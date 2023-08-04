318de7b8247753861e82bd8a85874791cc8a759f
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-04 23:32:03 +0000

    Create employee Entity

--- src/main/java/com/example/ems/Entity/Employee.java
+++ src/main/java/com/example/ems/Entity/Employee.java
@@ -22,7 +22,7 @@ public class Employee {
     @Column(name = "last_name")
     private String lastName;
     
-    @Column(name="email")
+    @Column(name="email_id", nullable = false, unique = true)
     private String email;
 
 }
