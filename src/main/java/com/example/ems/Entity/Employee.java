f2e1699f65b1c81bece8ff7097b24a3bdfaa2558
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-04 23:28:45 +0000

    Create employee Entity

--- src/main/java/com/example/ems/Entity/Employee.java
+++ src/main/java/com/example/ems/Entity/Employee.java
@@ -1,12 +1,19 @@
 package com.example.ems.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
+@Table(name = "employees")
 public class Employee {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
