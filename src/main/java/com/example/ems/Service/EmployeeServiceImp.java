3d289b55408f7db9e1e52345bdac3412070f953a
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-05 00:43:32 +0000

    Create method in Service

--- src/main/java/com/example/ems/Service/EmployeeServiceImp.java
+++ src/main/java/com/example/ems/Service/EmployeeServiceImp.java
@@ -1,6 +1,8 @@
 package com.example.ems.Service;
 
 import com.example.ems.Dto.EmployeeDto;
+import com.example.ems.Entity.Employee;
+import com.example.ems.Mapper.EmployeeMapper;
 import com.example.ems.Repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,12 +13,14 @@ public class EmployeeServiceImp implements EmployeeService{
 
     private EmployeeRepository repository;
 
-    public EmployeeServiceImp(EmployeeRepository repository) {
-        this.repository = repository;
-    }
-
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
-        return null;
+
+        Employee employee =  EmployeeMapper.mapToEmployee(employeeDto);
+
+        Employee savedEmployee = repository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
     }
+
 }
