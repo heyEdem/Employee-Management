ffb87c8775e760457064149cf215ef6aa28e0795
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-08 15:52:31 +0000

    frontend

--- src/main/java/com/example/ems/Controller/EmployeeRestController.java
+++ src/main/java/com/example/ems/Controller/EmployeeRestController.java
@@ -1,10 +1,28 @@
 package com.example.ems.Controller;
 
+import com.example.ems.Dto.EmployeeDto;
+import com.example.ems.Entity.Employee;
+import com.example.ems.Service.EmployeeServiceImp;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class EmployeeRestController {
-    public String findAll (){
-        return ""
+
+    private final EmployeeServiceImp service;
+
+    public EmployeeRestController(EmployeeServiceImp service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public String findAll(Model model){
+        List<EmployeeDto> all = service.getAllEmployees();
+        model.addAttribute("employees","all");
+        return "index";
     }
 }
