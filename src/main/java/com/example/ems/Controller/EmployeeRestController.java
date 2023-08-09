9479880b79c5d4b4ba60bab95e4fcedc398d709a
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-09 16:05:37 +0000

    Add Employee rest controller

--- src/main/java/com/example/ems/Controller/EmployeeRestController.java
+++ src/main/java/com/example/ems/Controller/EmployeeRestController.java
@@ -1,13 +1,11 @@
 package com.example.ems.Controller;
 
 import com.example.ems.Dto.EmployeeDto;
-import com.example.ems.Entity.Employee;
 import com.example.ems.Service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
@@ -28,15 +26,14 @@ public class EmployeeRestController {
         model.addAttribute("employees",employees);
         return "index";
     }
-    @GetMapping("/add-employee")
-    public String addEmployee(Model model){
+    @GetMapping("/addEmployee")
+    public String showForm(Model model, EmployeeDto employeeDto){
         return "add-employee";
     }
 
-    @PostMapping("/add-employee")
-    public String addEmployee(Model model, EmployeeDto employeeDto){
+    @PostMapping("/addEmployee")
+    public String addEmployee( EmployeeDto employeeDto){
         EmployeeDto employee = service.createEmployee(employeeDto);
-        model.addAttribute("employee",employee);
         return "redirect:/all";
     }
 }
