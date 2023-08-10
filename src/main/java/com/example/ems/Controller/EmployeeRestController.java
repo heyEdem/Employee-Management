a655069d7eeb69228daef33deb361e566de373ca
Edem Akato <104960106+heyEdem@users.noreply.github.com>
2023-08-10 00:13:39 +0000

    Delete Employee rest controller

--- src/main/java/com/example/ems/Controller/EmployeeRestController.java
+++ src/main/java/com/example/ems/Controller/EmployeeRestController.java
@@ -5,7 +5,9 @@ import com.example.ems.Service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import java.util.List;
@@ -20,7 +22,7 @@ public class EmployeeRestController {
         this.service = service;
     }
 
-    @GetMapping("/all")
+    @GetMapping("/")
     public String findAll (Model model, EmployeeDto employee){
         List<EmployeeDto> employees = service.getAllEmployees();
         model.addAttribute("employees",employees);
@@ -34,6 +36,12 @@ public class EmployeeRestController {
     @PostMapping("/addEmployee")
     public String addEmployee( EmployeeDto employeeDto){
         EmployeeDto employee = service.createEmployee(employeeDto);
-        return "redirect:/all";
+        return "redirect:/";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete( @PathVariable("id") Long id){
+        service.deleteEmployee(id);
+        return "redirect:/";
     }
 }
